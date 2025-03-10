f3c057a3cdb2f1a9f51758668448f79e831430f6
Uwaseedith <e.uwase@alustudent.com>
2025-03-10 14:18:56 +0200

    send email notification

--- BazaPrix/settings.py
+++ BazaPrix/settings.py
@@ -157,6 +157,7 @@ LOCALE_PATHS = [
 LANGUAGE_CODE = 'en'
 
 # Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
