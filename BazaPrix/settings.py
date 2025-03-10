bf680d7d0294e0649f0ce1e6560e4fa77275fde6
Uwaseedith <e.uwase@alustudent.com>
2025-03-10 14:35:17 +0200

    send email notification done

--- BazaPrix/settings.py
+++ BazaPrix/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,7 +160,6 @@ LOCALE_PATHS = [
 LANGUAGE_CODE = 'en'
 
 # Email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
